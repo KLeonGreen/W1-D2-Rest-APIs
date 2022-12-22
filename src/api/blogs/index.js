2b413046c9308163ebcc9176f2319982d0fcca1b
KLeonGreen <114649634+KLeonGreen@users.noreply.github.com>
2022-12-22 15:14:26 +0100

    validations & error handlers

--- src/api/blogs/index.js
+++ src/api/blogs/index.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import { dirname, join } from "path";
 import uniqid from "uniqid";
 import { fileURLToPath } from "url";
+import { checkBlogSchema, getbadRequest } from "./validator.js";
 
 const currentPath = import.meta.url;
 const currentPathtoURL = fileURLToPath(currentPath);
@@ -16,16 +17,51 @@ const getBlogs = () => {
 };
 
 const addBlogs = (array) => {
-  fs.writeFileSync(blogJSONfilePath, JSON.stringify(array));
+  return fs.writeFileSync(blogJSONfilePath, JSON.stringify(array));
 };
 
-blogRouter.post("/", (request, response) => {
-  const newBlog = { ...request.body, date: new Date(), id: uniqid() };
-  const blogArray = getBlogs();
-  blogArray.push(newBlog);
-  addBlogs(blogArray);
+blogRouter.post("/", checkBlogSchema, getbadRequest, (request, response, next) => {
+  try {
+    const newBlog = { ...request.body, date: new Date(), id: uniqid() };
+    const blogArray = getBlogs();
+    blogArray.push(newBlog);
+    addBlogs(blogArray);
 
-  response.status(201).send(newBlog);
+    response.status(201).send(newBlog);
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogRouter.get("/", (request, response) => {
+  const blogArray = getBlogs();
+  response.status(201).send(blogArray);
+});
+
+blogRouter.get("/:id", (request, response) => {
+  const id = request.params.id;
+  const blogArray = getBlogs();
+  const blog = blogArray.find((blog) => blog.id === id);
+  response.send(blog);
+});
+
+blogRouter.put("/:id", (request, response) => {
+  const id = request.params.id;
+  const blogArray = getBlogs();
+  const editedblogindex = blogArray.findIndex((blog) => blog.id === id);
+  const oldBlog = blogArray[editedblogindex];
+  const newBlog = { ...oldBlog, ...request.body, updated: new Date() };
+  blogArray[editedblogindex] = newBlog;
+  addBlogs(blogArray);
+  response.status(200).send(newBlog);
+});
+
+blogRouter.delete("/:id", (request, response) => {
+  const id = request.params.id;
+  const blogArray = getBlogs();
+  const remainingBlogs = blogArray.filter((blog) => blog.id !== id);
+  addBlogs(remainingBlogs);
+  response.status(200).send(remainingBlogs);
 });
 
 export default blogRouter;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,24 @@
 import express from "express";
 import authorsRouter from "./api/users/index.js";
-import blogRouter from "../src/api/blogs/index.js";
+import blogRouter from "./api/blogs/index.js";
 import listEndpoints from "list-endpoints-express";
+import cors from "cors";
+import { badRequestHandler, unauthorizedHandler, notFoundHandler, genericHandler } from "./errorHandler.js";
 
 const server = express();
-const port = 3009;
+const port = 3001;
 
 server.use(express.json());
 
+server.use(cors());
+
 server.use("/authors", authorsRouter);
 server.use("/blogs", blogRouter);
+
+server.use(badRequestHandler);
+server.use(unauthorizedHandler);
+server.use(notFoundHandler);
+server.use(genericHandler);
 
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
