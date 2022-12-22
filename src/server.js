7737d3c6eb6b7925878d52524416ac2065ed7d7f
KLeonGreen <114649634+KLeonGreen@users.noreply.github.com>
2022-12-22 17:14:14 +0100

    file upload

--- src/server.js
+++ src/server.js
@@ -1,19 +1,25 @@
 import express from "express";
 import authorsRouter from "./api/users/index.js";
 import blogRouter from "./api/blogs/index.js";
+import filesRouter from "./api/files/index.js";
 import listEndpoints from "list-endpoints-express";
 import cors from "cors";
+import { join } from "path";
 import { badRequestHandler, unauthorizedHandler, notFoundHandler, genericHandler } from "./errorHandler.js";
 
 const server = express();
 const port = 3001;
+const publicPATH = join(process.cwd(), "./public");
 
 server.use(express.json());
+
+server.use(express.static(publicPATH));
 
 server.use(cors());
 
 server.use("/authors", authorsRouter);
 server.use("/blogs", blogRouter);
+server.use("/cover", filesRouter);
 
 server.use(badRequestHandler);
 server.use(unauthorizedHandler);
